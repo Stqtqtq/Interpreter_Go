e82ae5f7c7bebab9013d96932548bd05c52faf37
TQ <tianquan95@gmail.com>
2024-06-22 14:49:23 +0800

    added eval and obj

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"src/evaluator"
 	"src/lexer"
+	"src/object"
 	"src/parser"
 )
 
@@ -12,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -30,10 +33,19 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
+
+//
+//		io.WriteString(out, program.String())
+//		io.WriteString(out, "\n")
+//	}
+//}
 
 const MONKEY_FACE = `            __,__
    .--.  .-"     "-.  .--.
